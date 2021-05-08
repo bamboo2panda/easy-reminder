88f85e5f4814c9d268bac2e45fc3e425ada9d3ae
Pavel <bamboo2panda@gmail.com>
2021-05-08 10:12:37 +0300

    Added frontend .env template.

--- docker/backend/production/wsgi-entrypoint.sh
+++ docker/backend/production/wsgi-entrypoint.sh
@@ -13,4 +13,10 @@ done
 
 ./manage.py collectstatic --noinput
 
+
+
+celery multi start w1 -A app -l INFO --pidfile=/var/run/celery/%n.pid \
+                                        --logfile=/var/log/celery/%n%I.log
+
+
 gunicorn app.wsgi --bind 0.0.0.0:8000 --workers 4 --threads 4
